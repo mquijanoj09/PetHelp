49ede6ecbaae5c8b4b9da8a336291d8c951c4d2b
johamja <johamfranco318@gmail.com>
2024-08-21 21:30:49 -0500

    Implementación, lógica para cambiar de página en un definido tiempo

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
-window.onload = function(){
+window.onload = function () {
+// Tiempo en milisegundos (por ejemplo, 5000 ms = 5 segundos)
+    const tiempo = 5000;
+
+// URL a la que quieres redirigir
+    const url = "./user-login.html";
+
+// Usamos setTimeout para esperar el tiempo especificado antes de redirigir
+    setTimeout(() => {
+        window.location.href = url;
+    }, tiempo);
 
 }
