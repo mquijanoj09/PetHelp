f1bbd5fc72301df6d19e9d67b416990bd1a35c53
johamja <johamfranco318@gmail.com>
2024-08-25 16:29:46 -0500

    Implementación, lógica de carga de las tarjetas de tipos de mascotas

--- src/js/home.js
+++ src/js/home.js
@@ -1,3 +1,30 @@
-window.onload = function(){
+import {tipoMascotas} from './tipo-mascotas.js';
 
+window.onload = function () {
+
+    // Referencia del elemento base donde se generan las tarjetas
+    const container = document.getElementById('guia-primeros-auxilios');
+
+    for (const Key in tipoMascotas) {
+
+        const newCard = document.createElement('div');
+        newCard.className = 'tarjeta-tipo-mascota Elevation2';
+
+        // Contenido de la tarjeta
+        newCard.innerHTML = `
+        <img src="${tipoMascotas[Key]['imagen']}" alt="${tipoMascotas[Key]['nombre']}">
+        <div class="descripcion">
+            <div class="textos">
+                <h2>${tipoMascotas[Key]['nombre']}</h2>
+                <h5 class="tag">${tipoMascotas[Key]['numero-emergencias']} Situaciones de emergencia</h5>
+                <h5 class="tag">${tipoMascotas[Key]['numero-soluciones']} Soluciones rápidas</h5>
+            </div>
+            <div class="box-boton">
+                <button class="boton-c-emergencia">Emergencias</button>
+            </div>
+        </div>`;
+
+        // Añadimos la tarjeta como hija del contenedor
+        container.appendChild(newCard);
+    }
 }
---
+++ src/js/tipo-mascotas.js
@@ -0,0 +1,24 @@
+import Perro from '../img/Home/Perro.png'
+import Gato from '../img/Home/Gato.png'
+import Roedor from '../img/Home/Roedor.png'
+
+export const tipoMascotas = [
+    {
+        "nombre": "Perros",
+        "numero-emergencias": 2,
+        "numero-soluciones": 4,
+        "imagen": Perro
+    },
+    {
+        "nombre": "Gatos",
+        "numero-emergencias": 12,
+        "numero-soluciones": 3,
+        "imagen": Gato
+    },
+    {
+        "nombre": "Roedores",
+        "numero-emergencias": 2,
+        "numero-soluciones": 4,
+        "imagen": Roedor
+    }
+];
