417b0e14e464593789750acedcaaab5c0fe1e340
johamja <johamfranco318@gmail.com>
2024-08-25 17:18:08 -0500

    Implementación, lógica de carga de los consejos en el home

--- src/js/home.js
+++ src/js/home.js
@@ -1,9 +1,12 @@
 import {tipoMascotas} from './tipo-mascotas.js';
+import {consejos} from './consejos';
 
 window.onload = function () {
 
     // Referencia del elemento base donde se generan las tarjetas
     const container = document.getElementById('guia-primeros-auxilios');
+    const consejosColumna1 = document.getElementById('columna-1');
+    const consejosColumna2 = document.getElementById('columna-2');
 
     for (const Key in tipoMascotas) {
 
@@ -26,5 +29,31 @@ window.onload = function () {
 
         // Añadimos la tarjeta como hija del contenedor
         container.appendChild(newCard);
+    }
+
+    for (const Key in consejos) {
+        const newCard = document.createElement('div');
+
+        let opciones = ['tarjeta-consejo-c-primary', 'tarjeta-consejo-c-secondary', 'tarjeta-consejo-c-onsurface']
+        newCard.className = opciones[Math.floor(Math.random() * opciones.length)];
+
+        // Contenido de la tarjeta
+        newCard.innerHTML = `
+            <h3>${consejos[Key]['nombre']}</h3>
+            <p style="font-style: italic">${consejos[Key]['consejo']}</p>`;
+
+        let imagen = `<div>`;
+        for (const Key2 in consejos[Key]['tipo_mascota']) {
+            imagen += `<img class="imagen-tipo-consejo" src="${consejos[Key]['tipo_mascota'][Key2]}" alt="Perro">`
+        }
+
+        newCard.innerHTML += imagen + '</div>';
+
+        // Añadimos la tarjeta como hija del contenedor
+        if (Key < consejos.length / 2) {
+            consejosColumna1.appendChild(newCard);
+        } else {
+            consejosColumna2.appendChild(newCard);
+        }
     }
 }
